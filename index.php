0f4727cf8ac8a9497a6587746d46b4ea9059ce68
loki <101629602+Wtrochimiuk@users.noreply.github.com>
2022-09-14 11:43:36 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+</head>
+     <body>
+          
+     <body bgcolor="FBD7BB">
+     <h1>yoyoyo</h1>
+
+</body>
+        
+          <?
+          $counter = 1;
+          $liczba = 5;
+
+          echo "Masz" $liczba "wiadomosci" <br>";
+        
+          if($msg == 0);
+          {
+            echo "masz 0 wiadomosci <br>";
+          }
+          else
+          {
+            echo "masz $msg wiadomosci <br>";
+          }
+        
+
+        
+    
+        
+
+
+</html>
